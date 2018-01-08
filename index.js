7a34f5958aafe193f9612cc5d569f558518494fd
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-08 03:12:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (string.toLowerCase() === string)
 {
   return"I can't hear you!"
 }
-else if (string.toUpperCase()===strong)
+else if (string.toUpperCase()===string)
 {
  return "YES INDEED!"
 }
