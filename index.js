02348e065ac257aafdd626a570a66ef33cf7d1bb
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-08 03:11:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ function sayHiToGrandma(string)
 {
 if (string.toLowerCase() === string)
 {
-console.log("I can't hear you!")
+  return"I can't hear you!"
 }
 if (string.toUpperCase()===strong)
 {
- console.log("YES INDEED!")
+ return "YES INDEED!"
 }
 if (string==="I love you, Grandma")
 {
-  console.log( "I love you, too.")
+  return "I love you, too."
 }
 }
 
